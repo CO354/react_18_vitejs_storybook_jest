e8dfccc24de3694650d97bd1e19b57929bf1d8dc
anselmo <anselmoahmed@gmail.com>
2023-08-16 09:50:08 -0300

    react-styled-components-storybook

---
+++ src/templates/App/stories.jsx
@@ -0,0 +1,16 @@
+import { App } from '.';
+
+export default {
+  title: 'App',
+  component: App,
+  args: {
+    children: 'Children padrão',
+  },
+  argTypes: {
+    children: { type: 'string' },
+  },
+};
+
+export const Template = (args) => <App {...args} />;
+
+
